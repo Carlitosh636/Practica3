b883e6179fbcf27580594cd720a5a8e8aa26ad7e
Carlitosh636 <carloshervas63@hotmail.com>
2022-05-02 17:08:20 +0200

    dialog

--- js/dialog.js
+++ js/dialog.js
@@ -1,31 +1,52 @@
 
 var modal = document.getElementById("modal");
-var modalText = document.getElementById("modalText")
-var modalImg = document.getElementById("modalImg")
-
-
-
-
-
+var modalText = document.getElementById("dialog-title")
+var modalDesc = document.getElementById("dialog-desc")
+var modalImg = document.getElementById("dialog-img")
+var disableHandle,tabHandle,lastFocus,hiddenHandle;
 var span = document.getElementsByClassName("close")[0];
 
 
-function activateModal(text) {
+function activateModal(text,imgUrl,desc) {
   modal.style.display = "block";
-  modalText.innerHTML=text
+  modalText.innerHTML=text;
+  modalImg.src=imgUrl;
+  modalDesc.innerText=desc;
+  lastFocus = document.activeElement;
+  disableHandle = ally.maintain.disabled({
+    filter:modal,
+  });
+  tabHandle = ally.maintain.tabFocus({
+    context: modal,
+  });
+  var element = ally.query.firstTabbable({
+    context: modal,
+    defaultToContext: true,
+  });
+  hiddenHandle = ally.maintain.hidden({
+    filter: modal,
+  });
+  element.focus();
+}
+function deactivateModal(){
+  disableHandle.disengage();
+  tabHandle.disengage();
+  hiddenHandle.disengage();
+  lastFocus.focus();
+  modal.style.display="none"
 }
 
 span.onclick = function() {
-  modal.style.display = "none";
+  deactivateModal()
 }
 
 window.onclick = function(event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    deactivateModal()
   }
 }
 document.addEventListener('keydown',function(event){
   if (event.key == "Escape"){
-    modal.style.display="none"
+    deactivateModal()
   }
 })
